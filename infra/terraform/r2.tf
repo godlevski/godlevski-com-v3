14b8374ac45cd0c7c345b2f036c37fc9907ec65b
Dmitriy Godlevski <d.godlevski@gmail.com>
2026-08-15 14:43:58 -0500

    r2 on terraform

--- infra/terraform/r2.tf
+++ infra/terraform/r2.tf
@@ -8,3 +8,10 @@ resource "cloudflare_r2_bucket" "art_godlevski_web" {
   account_id = var.cloudflare_account_id
   name       = "art-godlevski-web"
 }
+
+# public files for both sites, served by the godlevski-files worker
+# (slides originals, shapefiles, assorted; synced via infra/sync-files.sh)
+resource "cloudflare_r2_bucket" "godlevski_files" {
+  account_id = var.cloudflare_account_id
+  name       = "godlevski-files"
+}
